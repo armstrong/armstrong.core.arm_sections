a427fab365ff3404748cdf8fb70b4ff542e81922
Travis Swicegood <development@domain51.com>
2011-05-16 18:31:23 -0500

    Update to use the spec command in the latest armstrong.dev

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,4 @@
 from armstrong.dev.tasks import *
-from d51.django.virtualenv.base import VirtualEnvironment
 from fabric.api import task
 
 
@@ -21,20 +20,6 @@ settings = {
     'SITE_ID': 1,
 }
 
+full_name = "armstrong.core.arm_sections"
 main_app = "arm_sections"
 tested_apps = (main_app, )
-
-@task
-def lettuce(verbosity=4):
-    defaults = settings
-    defaults["DATABASES"] = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": ":memory:",
-        },
-    }
-    v = VirtualEnvironment()
-    v.run(defaults)
-    v.call_command("syncdb", interactive=False)
-    v.call_command("harvest", apps='armstrong.core.arm_sections',
-            verbosity=verbosity)
