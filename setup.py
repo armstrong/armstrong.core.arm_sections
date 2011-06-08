88cd8734b2643e9b713ac6400a098fc850bb514d
Travis Swicegood <development@domain51.com>
2011-06-08 15:49:40 -0500

    bump for doc dev version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ def build_package(dirpath, dirnames, filenames):
 
 setup(
     name='armstrong.core.arm_sections',
-    version='0.1.2',
+    version='0.1.2.0.alpha.0',
     description='Provides the basic section objects',
     author='Bay Citizen & Texas Tribune',
     author_email='dev@armstrongcms.org',
