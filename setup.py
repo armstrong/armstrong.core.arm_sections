1b811234dc3a4d945b1a71194fc76d9030e07ae0
Travis Swicegood <development@domain51.com>
2011-08-10 14:43:22 -0500

    Revert "Refactor to rely on latest release of Idios" (i.e., I'm a space cadet)
    This reverts commit 9583e95bf5e8606bcd982ba4946e35d16f0e72bc.

--- setup.py
+++ setup.py
@@ -85,6 +85,7 @@ setup_kwargs = {
     "packages": packages,
     "package_data": {info["name"]: data_files, },
     "namespace_packages": NAMESPACE_PACKAGES,
+    "setup_requires": ["pip", "distribute", ],
     "classifiers": [
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
@@ -98,3 +99,9 @@ setup_kwargs = {
 
 setup_kwargs.update(info)
 setup(**setup_kwargs)
+
+import sys
+if "install" in sys.argv:
+    import pip
+    pip.main(["install", "-r", "requirements/base.txt", "-b",
+              "build-%s" % info["name"]])
