014970212c0bbb95d61f12622c5601f6dc83ab99
Niran Babalola <niran@niran.org>
2012-01-23 22:23:32 -0600

    Break apart backend tests into separate classes.

--- armstrong/core/arm_sections/tests/backends.py
+++ armstrong/core/arm_sections/tests/backends.py
@@ -4,15 +4,15 @@ from ._utils import *
 from ..models import Section
 from arm_sections_support.models import *
 
-class SectionBackendTestCase(ArmSectionsTestCase):
+
+class ManyToManyBackendTestCase(ArmSectionsTestCase):
     """
+    Test fetching items for content with a many-to-many relationship to sections.
     """
     def setUp(self):
-        super(SectionBackendTestCase, self).setUp()
+        super(ManyToManyBackendTestCase, self).setUp()
 
         self.sports = Section.objects.get(slug='sports')
-        self.pro_sports = Section.objects.get(slug='pro')
-        self.weather = Section.objects.get(slug='weather')
 
         self.article = Article.objects.create(
                 title="Test Article",
@@ -24,6 +24,23 @@ class SectionBackendTestCase(ArmSectionsTestCase):
                 slug='second_article',
             )
         self.article2.sections = [self.sports]
+
+    def test_backend_with_articles(self):
+        with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.Common'):
+            self.assert_(self.article in self.sports.items)
+            self.assert_(self.article2 in self.sports.items)
+
+
+class ForeignKeyBackendTestCase(ArmSectionsTestCase):
+    """
+    Test fetching items for content with a foreign key relationship to sections.
+    """
+    def setUp(self):
+        super(ForeignKeyBackendTestCase, self).setUp()
+
+        self.sports = Section.objects.get(slug='sports')
+        self.weather = Section.objects.get(slug='weather')
+
         self.foreign_key_article = SectionForeignKeyArticle.objects.create(
                 title="Test Foreign Key Article",
                 slug='test_foreign_key_article',
@@ -34,6 +51,23 @@ class SectionBackendTestCase(ArmSectionsTestCase):
                 slug='second_foreign_key',
                 primary_section=self.weather
             )
+
+    def test_backend_with_foreign_key_articles(self):
+        with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.SectionForeignKeyCommon'):
+            self.assert_(self.foreign_key_article in self.sports.items)
+            self.assert_(self.foreign_key_article2 in self.weather.items)
+
+
+class ComplexBackendTestCase(ArmSectionsTestCase):
+    """
+    Test fetching items for content with foreign key and many-to-many relationships to sections.
+    """
+    def setUp(self):
+        super(ComplexBackendTestCase, self).setUp()
+
+        self.pro_sports = Section.objects.get(slug='pro')
+        self.weather = Section.objects.get(slug='weather')
+
         self.complex_article = ComplexArticle.objects.create(
                 title="Test Complex Article",
                 slug='test_complex_article',
@@ -41,20 +75,28 @@ class SectionBackendTestCase(ArmSectionsTestCase):
             )
         self.complex_article.related_sections = [self.weather]
 
-    def test_backend_with_articles(self):
-        with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.Common'):
-            self.assert_(self.article in self.sports.items)
-            self.assert_(self.article2 in self.sports.items)
-
-    def test_backend_with_foreign_key_articles(self):
-        with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.SectionForeignKeyCommon'):
-            self.assert_(self.foreign_key_article in self.sports.items)
-            self.assert_(self.foreign_key_article2 in self.weather.items)
-
     def test_backend_with_complex_articles(self):
         with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.ComplexCommon'):
             self.assert_(self.complex_article in self.pro_sports.items)
             self.assert_(self.complex_article in self.weather.items)
+
+class HierarchyBackendTestCase(ArmSectionsTestCase):
+    """
+    Test fetching items for a parent section of the associated section.
+    """
+    def setUp(self):
+        super(HierarchyBackendTestCase, self).setUp()
+
+        self.sports = Section.objects.get(slug='sports')
+        self.pro_sports = Section.objects.get(slug='pro')
+        self.weather = Section.objects.get(slug='weather')
+
+        self.complex_article = ComplexArticle.objects.create(
+                title="Test Complex Article",
+                slug='test_complex_article',
+                primary_section=self.pro_sports
+            )
+        self.complex_article.related_sections = [self.weather]
 
     def test_backend_with_section_hierarchy(self):
         with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.ComplexCommon'):
