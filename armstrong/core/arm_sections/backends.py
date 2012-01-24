c474f3b8c8f2dcae722598f979fd544f42b2d922
Travis Swicegood <development@domain51.com>
2012-01-24 13:26:05 -0600

    Otherwise, these are going to haunt me in my sleep

--- armstrong/core/arm_sections/backends.py
+++ armstrong/core/arm_sections/backends.py
@@ -6,11 +6,11 @@ class ItemFilter(object):
     manager_attr = '_default_manager'
 
     def get_manager(self, model):
-        '''Return the desired manager for the item model.'''
+        """Return the desired manager for the item model."""
         return getattr(model, self.manager_attr)
 
     def get_section_relations(self, section):
-        '''Find every relationship between section and the item model.'''
+        """Find every relationship between section and the item model."""
         all_rels = section._meta.get_all_related_objects() + \
                    section._meta.get_all_related_many_to_many_objects()
         model_rels = []
@@ -22,7 +22,7 @@ class ItemFilter(object):
         return model_rels
 
     def filter_objects_by_section(self, rels, section):
-        '''Build a queryset containing all objects in the section subtree.'''
+        """Build a queryset containing all objects in the section subtree."""
         subtree = section.get_descendants(include_self=True)
         kwargs_list = [{'%s__in' % rel.field.name: subtree} for rel in rels]
         q = Q(**kwargs_list[0])
@@ -31,11 +31,12 @@ class ItemFilter(object):
         return self.get_manager(rels[0].model).filter(q)
 
     def process_items(self, items):
-        '''Perform extra actions on the filtered items.
+        """
+        Perform extra actions on the filtered items.
 
         Example: Further filtering the items in the section to meet a
         custom need.
-        '''
+        """
         if hasattr(items, 'select_subclasses'):
             items = items.select_subclasses()
         return items
