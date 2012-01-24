fb8cebe48117d9c7d900a112ed700b000c110e51
Niran Babalola <niran@niran.org>
2012-01-24 13:11:24 -0600

    Add docstrings to ItemFilter

--- armstrong/core/arm_sections/backends.py
+++ armstrong/core/arm_sections/backends.py
@@ -5,7 +5,12 @@ from django.db.models import Q
 class ItemFilter(object):
     manager_attr = '_default_manager'
 
+    def get_manager(self, model):
+        '''Return the desired manager for the item model.'''
+        return getattr(model, self.manager_attr)
+
     def get_section_relations(self, section):
+        '''Find every relationship between section and the item model.'''
         all_rels = section._meta.get_all_related_objects() + \
                    section._meta.get_all_related_many_to_many_objects()
         model_rels = []
@@ -16,10 +21,8 @@ class ItemFilter(object):
                 model_rels.append(related)
         return model_rels
 
-    def get_manager(self, model):
-        return getattr(model, self.manager_attr)
-
     def filter_objects_by_section(self, rels, section):
+        '''Build a queryset containing all objects in the section subtree.'''
         subtree = section.get_descendants(include_self=True)
         kwargs_list = [{'%s__in' % rel.field.name: subtree} for rel in rels]
         q = Q(**kwargs_list[0])
@@ -28,6 +31,11 @@ class ItemFilter(object):
         return self.get_manager(rels[0].model).filter(q)
 
     def process_items(self, items):
+        '''Perform extra actions on the filtered items.
+
+        Example: Further filtering the items in the section to meet a
+        custom need.
+        '''
         if hasattr(items, 'select_subclasses'):
             items = items.select_subclasses()
         return items
