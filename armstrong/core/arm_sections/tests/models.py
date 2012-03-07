24ab206ffa68d145cd9295a153086c265cef9a2c
Niran Babalola <niran@niran.org>
2012-03-07 12:24:54 -0600

    Add sanity check messages to tests

--- armstrong/core/arm_sections/tests/models.py
+++ armstrong/core/arm_sections/tests/models.py
@@ -23,25 +23,25 @@ class ModelTestCase(ArmSectionsTestCase):
 
     def test_can_ensure_section(self):
         with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.ComplexCommon'):
-            self.assert_(self.complex_article not in self.college.items)
+            self.assert_(self.complex_article not in self.college.items, msg='sanity check')
             self.college.add_item(self.complex_article)
             self.assert_(self.complex_article in self.college.items)
 
     def test_can_remove_section(self):
         with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.ComplexCommon'):
-            self.assert_(self.complex_article in self.weather.items)
+            self.assert_(self.complex_article in self.weather.items, msg='sanity check')
             self.weather.remove_item(self.complex_article)
             self.assert_(self.complex_article not in self.weather.items)
 
     def test_can_set_section_with_true_test_func(self):
         with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.ComplexCommon'):
-            self.assert_(self.complex_article not in self.college.items)
+            self.assert_(self.complex_article not in self.college.items, msg='sanity check')
             self.college.set_item(self.complex_article, lambda x: True)
             self.assert_(self.complex_article in self.college.items)
 
     def test_can_set_section_with_false_test_func(self):
         with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.ComplexCommon'):
-            self.assert_(self.complex_article in self.weather.items)
+            self.assert_(self.complex_article in self.weather.items, msg='sanity check')
             self.weather.set_item(self.complex_article, lambda x: False)
             self.assert_(self.complex_article not in self.weather.items)
 
@@ -63,24 +63,24 @@ class ManagerTestCase(ArmSectionsTestCase):
 
     def test_can_ensure_section_id(self):
         with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.ComplexCommon'):
-            self.assert_(self.complex_article not in self.college.items)
+            self.assert_(self.complex_article not in self.college.items, msg='sanity check')
             Section.objects.add_item(self.college.pk, self.complex_article)
             self.assert_(self.complex_article in self.college.items)
 
     def test_can_remove_section_id(self):
         with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.ComplexCommon'):
-            self.assert_(self.complex_article in self.weather.items)
+            self.assert_(self.complex_article in self.weather.items, msg='sanity check')
             Section.objects.remove_item(self.weather.pk, self.complex_article)
             self.assert_(self.complex_article not in self.weather.items)
 
     def test_can_set_section_id_with_true_test_func(self):
         with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.ComplexCommon'):
-            self.assert_(self.complex_article not in self.college.items)
+            self.assert_(self.complex_article not in self.college.items, msg='sanity check')
             Section.objects.set_item(self.college.pk, self.complex_article, lambda x: True)
             self.assert_(self.complex_article in self.college.items)
 
     def test_can_set_section_id_with_false_test_func(self):
         with override_settings(ARMSTRONG_SECTION_ITEM_MODEL='armstrong.core.arm_sections.tests.arm_sections_support.models.ComplexCommon'):
-            self.assert_(self.complex_article in self.weather.items)
+            self.assert_(self.complex_article in self.weather.items, msg='sanity check')
             Section.objects.set_item(self.weather.pk, self.complex_article, lambda x: False)
             self.assert_(self.complex_article not in self.weather.items)
