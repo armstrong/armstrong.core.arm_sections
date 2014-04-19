25c07381e45448bd2d89269e6a011f5aa95d9e53
Jon Cotton <jc@finaltransfer.net>
2014-04-18 20:03:54 -0700

    update migration to the output from a newer South (0.8.4)
    There's nothing here that changes our practice as South supports Django 1.2+. Unicode can't hurt and `db_index` on SlugField has been implied since Django 1.2 (maybe earlier).

--- armstrong/core/arm_sections/migrations/0001_initial.py
+++ armstrong/core/arm_sections/migrations/0001_initial.py
@@ -1,48 +1,47 @@
-# encoding: utf-8
-import datetime
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
 
+
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        
         # Adding model 'Section'
-        db.create_table('arm_sections_section', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'arm_sections_section', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
             ('summary', self.gf('django.db.models.fields.TextField')(default='', blank=True)),
-            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50, db_index=True)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(max_length=50)),
             ('full_slug', self.gf('django.db.models.fields.CharField')(max_length=255, blank=True)),
             ('parent', self.gf('mptt.fields.TreeForeignKey')(to=orm['arm_sections.Section'], null=True, blank=True)),
-            ('lft', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
-            ('rght', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
-            ('tree_id', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
-            ('level', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            (u'lft', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            (u'rght', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            (u'tree_id', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
+            (u'level', self.gf('django.db.models.fields.PositiveIntegerField')(db_index=True)),
         ))
-        db.send_create_signal('arm_sections', ['Section'])
+        db.send_create_signal(u'arm_sections', ['Section'])
 
 
     def backwards(self, orm):
-        
         # Deleting model 'Section'
-        db.delete_table('arm_sections_section')
+        db.delete_table(u'arm_sections_section')
 
 
     models = {
-        'arm_sections.section': {
+        u'arm_sections.section': {
             'Meta': {'object_name': 'Section'},
             'full_slug': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'parent': ('mptt.fields.TreeForeignKey', [], {'to': "orm['arm_sections.Section']", 'null': 'True', 'blank': 'True'}),
-            'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
-            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'level': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            u'lft': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'parent': ('mptt.fields.TreeForeignKey', [], {'to': u"orm['arm_sections.Section']", 'null': 'True', 'blank': 'True'}),
+            u'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
             'summary': ('django.db.models.fields.TextField', [], {'default': "''", 'blank': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
+            u'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
         }
     }
 
